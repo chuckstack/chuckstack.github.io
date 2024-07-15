bf9f9a4ec0f4cdcf1564ceab330e0883e6f40328
Chuck Boecking <chuck@chuboe.com>
2024-07-15 11:35:52 -0500

    renamed navigation embed

--- js/load-common-elements.js
+++ js/load-common-elements.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  fetch('navigation.html')
+  fetch('embed-navigation.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('navigation-placeholder').innerHTML = data;
