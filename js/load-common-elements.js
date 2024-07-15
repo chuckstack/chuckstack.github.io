8d3222322e19ac31fe0854a20f77ec64f3532387
Chuck Boecking <chuck@chuboe.com>
2024-07-15 10:25:56 -0500

    dry - limit dry to only navigation details

--- js/load-common-elements.js
+++ js/load-common-elements.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  fetch('header.html')
+  fetch('navigation.html')
     .then(response => response.text())
     .then(data => {
-      document.getElementById('header-placeholder').innerHTML = data;
-    });
-
-  fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('footer-placeholder').innerHTML = data;
+      document.getElementById('navigation-placeholder').innerHTML = data;
     });
 });
