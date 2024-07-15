e4ce7711c3fd043e1a91e6a8a2f739e3a7090604
Chuck Boecking <chuck@chuboe.com>
2024-07-15 11:25:57 -0500

    form - embed as dry component

--- js/load-common-elements.js
+++ js/load-common-elements.js
@@ -6,4 +6,10 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById('navigation-placeholder').innerHTML = data;
     });
 
+  fetch('embed-form-stay-in-touch.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('stay-in-touch-placeholder').innerHTML = data;
+    });
+
 });
